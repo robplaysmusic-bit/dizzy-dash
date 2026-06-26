de1eeea82b14f02668646ce00d494bc89bafe652
Rob Poliseno <robplaysmusic@gmail.com>
2026-06-25 22:16:54 -0400

    Updates for feel
    Camera more zoomed in. Some tweaks to results UI.

--- menus/result_row.gd
+++ menus/result_row.gd
@@ -1,11 +1,10 @@
 class_name ResultRow extends Control
 
-@onready var left: Label = $Label
-@onready var right: Label = $Label2
+@onready var left: Label = $Label2
+@onready var right: Label = $Label
 
 func set_text(description: String, time: float, color: Color) -> void:
-    left.add_theme_color_override("font_color", color)
-    right.add_theme_color_override("font_color", color)
-    left.text = description
-    right.text = DizzyManager.format_time(time)
-
+	left.add_theme_color_override("font_color", color)
+	right.add_theme_color_override("font_color", color)
+	left.text = description
+	right.text = DizzyManager.format_time(time)
