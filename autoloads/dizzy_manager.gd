7f703d9da855b3ce7cd2fef804f4986659b468fb
Max <silverhammermba@gmail.com>
2026-06-26 20:56:43 -0400

    fix dizziness variance
    it was too slow and jumped around when looping

--- autoloads/dizzy_manager.gd
+++ autoloads/dizzy_manager.gd
@@ -10,26 +10,27 @@ const MAX_EXTREMELY_DIZZY : int = 25
 const MAX_OH_NO_DIZZY : int = 5000
 
 class DizzyEffect:
-	var angle_range: float
-	var min_angle: float
-	var max_angle: float
-	var angle_change_rate: float
+	## the effect generally rotates input by this much
+	var angle_center: float
+	## the effect rotates + or - this much
+	var angle_half_width: float
+	## how long it takes input rotation to oscillate
+	var angle_change_duration: float
 
-	func _init(center: float, width: float, rate: float) -> void:
-		self.angle_range = width
-		self.min_angle = center - width
-		self.max_angle = center + width
-		self.angle_change_rate = rate
+	func _init(center: float, half_width: float, duration: float) -> void:
+		self.angle_center = center
+		self.angle_half_width = half_width
+		self.angle_change_duration = duration
 
 
 var _info : Dictionary[Dizzy, DizzyEffect] = {
-	Dizzy.NOT_DIZZY: DizzyEffect.new(  0,  0, 0.0),
-	Dizzy.SLIGHTLY : DizzyEffect.new(180,  2, 0.0),
-	Dizzy.SOMEWHAT : DizzyEffect.new( 45,  2, 0.0),
-	Dizzy.STANDARD : DizzyEffect.new( 45, 13, 0.0),
-	Dizzy.VERY     : DizzyEffect.new( 75, 10, 1.0),
-	Dizzy.EXTREMELY: DizzyEffect.new( 90,  5, 0.5),
-	Dizzy.OH_NO    : DizzyEffect.new( 90, 10, 0.1)
+	Dizzy.NOT_DIZZY: DizzyEffect.new(  0,  0, 6.0),
+	Dizzy.SLIGHTLY : DizzyEffect.new(180,  2, 6.0),
+	Dizzy.SOMEWHAT : DizzyEffect.new( 45,  2, 6.0),
+	Dizzy.STANDARD : DizzyEffect.new( 45, 13, 6.0),
+	Dizzy.VERY     : DizzyEffect.new( 75, 10, 5.0),
+	Dizzy.EXTREMELY: DizzyEffect.new( 90,  5, 4.0),
+	Dizzy.OH_NO    : DizzyEffect.new( 90, 10, 3.0)
 }
 
 var _tier : Dizzy = Dizzy.NOT_DIZZY
@@ -61,6 +62,8 @@ var rotation_increasing : bool = true
 # Dizziness should be set before loading the next level
 func set_dizziness(value : int) -> void:
 	_dizziness = value
+	# restart timer to change its wait time
+	rotation_change_timer.start(_info[_tier].angle_change_duration)
 
 func get_dizziness() -> float:
 	return _dizziness
@@ -85,9 +88,13 @@ func get_dizziness_tier() -> String:
 # Intended to be called every physics processing frame to "correct" player inputs
 func apply_dizziness(player_input : Vector2) -> Vector2:
 	var effect := _info[_tier]
-	# TODO: this cycles but with a discontinuity at 0. make it smooth
-	var timer_progress := rotation_change_timer.time_left / rotation_change_timer.wait_time
-	var rotation_angle := effect.min_angle + timer_progress * effect.angle_range
+	var time_proportion_remaining := 0.0
+	if rotation_change_timer.wait_time > 0:
+		# loops 1 smoothly down to 0 (then instantly back to 1)
+		time_proportion_remaining = rotation_change_timer.time_left / rotation_change_timer.wait_time
+	# loops smoothly 0 to -1 to 0 to 1 to 0
+	var variance := sin(time_proportion_remaining * PI * 2)
+	var rotation_angle := effect.angle_center + variance * effect.angle_half_width
 
 	return player_input.rotated(deg_to_rad(rotation_angle))
 
