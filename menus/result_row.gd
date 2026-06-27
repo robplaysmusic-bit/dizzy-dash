836cefbc718c69c65935856477fc7ef9aa66556b
Max <silverhammermba@gmail.com>
2026-06-26 22:01:38 -0400

    font shadows everywhere
    fix confusing variables in results screen. hide debug UI

--- menus/result_row.gd
+++ menus/result_row.gd
@@ -1,11 +1,11 @@
 class_name ResultRow extends Control
 
-@onready var left: Label = $HBoxContainer/TimeLabel
-@onready var right: Label = $HBoxContainer/TierLabel
+@onready var time: Label = $HBoxContainer/TimeLabel
+@onready var tier: Label = $HBoxContainer/TierLabel
 @onready var hbox: HBoxContainer = $HBoxContainer
 
-func set_text(description: String, time: float, color: Color) -> void:
-	left.add_theme_color_override("font_color", color)
-	right.add_theme_color_override("font_color", color)
-	left.text = DizzyManager.format_time(time)
-	right.text = description
+func set_text(description: String, t: float, color: Color) -> void:
+	time.add_theme_color_override("font_color", color)
+	tier.add_theme_color_override("font_color", color)
+	time.text = DizzyManager.format_time(t)
+	tier.text = description
